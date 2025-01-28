f2f91f74af46dbaa9f34626f4c11bcd7b1268f84
talktojoegee <talktojoegee@gmail.com>
2025-01-28 11:28:34 +0100

    changed

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 Route::get('/migration', function(){
-    \Illuminate\Support\Facades\Artisan::call('config:cache');
+    \Illuminate\Support\Facades\Artisan::call('migrate');
     return dd('success');
 });
 
